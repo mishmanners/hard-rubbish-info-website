cec57527099af09b7f81bd32d3ba77e5f53f6acd
Michelle "MishManners®™" Duke <36594527+mishmanners@users.noreply.github.com>
2025-01-14 17:03:46 +1100

    add all images

--- src/tests/index_test.spec.js
+++ src/tests/index_test.spec.js
@@ -6,3 +6,9 @@ test('home page should load and have the "repurposeful" h1 heading', async ({ pa
     const heading = await page.locator('h1');
     await expect(heading).toHaveText('RePurposeful');
 });
+
+test('website should load without errors', async ({ page }) => {
+    const response = await page.goto('http://localhost:8080/index');
+    expect(response.ok()).toBeTruthy();
+    expect(page.url()).toBe('http://localhost:8080/index');
+});
